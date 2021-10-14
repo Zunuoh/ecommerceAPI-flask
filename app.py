65ed2d0d239390055b242e74951c67d5ca267bfa
Zunuoh <audponu@gmail.com>
2021-10-14 15:27:25 +0000

    First commit

---
+++ app.py
@@ -0,0 +1,37 @@
+from enum import unique
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+import os
+
+app = Flask(__name__)
+
+@app.route('/', methods = ['GET'])
+def get():
+    return jsonify({"msg":"hi"})
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///' + os.path.join(basedir,'db.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == 'main':
+    app.run(debug=True)
